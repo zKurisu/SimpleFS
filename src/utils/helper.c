d42f8af28aadec4735a41dde93a39888d66464da
Jie Orkarin <2694783083@qq.com>
2025-11-14 16:56:14 +0800

    feat: add zero helper function

---
+++ src/utils/helper.c
@@ -0,0 +1,10 @@
+/*
+ * helper.c
+ * Helper functions
+ * Copyright (C) Jie
+ * 2025-11-14
+ *
+ */
+
+#include "helper.h"
+
---
+++ src/utils/helper.h
@@ -0,0 +1,14 @@
+/*
+ * helper.h
+ * Helper function signatures
+ * Copyright (C) Jie
+ * 2025-11-14
+ *
+ */
+
+#ifndef MY_HELPER_H_
+#define MY_HELPER_H_
+#include <string.h>
+#define zero(str,size) memset(str,0,size)
+
+#endif
