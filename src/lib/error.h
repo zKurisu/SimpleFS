9b5b6037f1fa289535cbfa757dc06b4fc710ca6f
Jie Orkarin <2694783083@qq.com>
2025-11-14 15:21:08 +0800

    feat: error code support

---
+++ src/lib/error.h
@@ -0,0 +1,19 @@
+/*
+ * error.h
+ * Error enum
+ * Copyright (C) Jie
+ * 2025-11-14
+ *
+ */
+#ifndef MY_ERROR_H_
+#define MY_ERROR_H_
+
+typedef enum {
+    OK,
+    ErrDread,
+    ErrDwrite,
+    ErrDattach,
+    ErrDdetach
+} RC; // Return Code
+
+#endif
