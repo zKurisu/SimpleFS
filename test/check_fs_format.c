3478237ed3b0020121f3641a9f6a497bbf5934d7
Jie Orkarin <2694783083@qq.com>
2025-11-17 18:32:07 +0800

    test: test for fs format, mount, unmount and show

--- test/check_fs_format.c
+++ test/check_fs_format.c
@@ -7,6 +7,7 @@ int main(void) {
     uint32_t size, n;
     uint32_t rw_size = 255;
     RC ret;
+    filesystem *fs;
 
     size = sizeof(struct s_disk);
     dd = (disk*)malloc(size);
@@ -27,6 +28,37 @@ int main(void) {
         free(dd);
         exit(-1);
     }
+    printf("Success to format disk...\n");
+
+    size = sizeof(struct s_filesystem);
+    fs = (filesystem *)malloc(size);
+    ret = fs_mount(dd, fs);
+    if (ret != OK) {
+        fprintf(stderr, "Failed to mount filesystem: %d\n", ret);
+        free(dd);
+        free(fs);
+        exit(-1);
+    }
+    printf("Success to mount disk to filesystem...\n");
+
+    ret = fs_show(fs);
+    if (ret != OK) {
+        fprintf(stderr, "Failed to show filesystem: %d\n", ret);
+        free(dd);
+        free(fs);
+        exit(-1);
+    }
+    printf("Success to show filesystem info...\n");
+
+    ret = fs_unmount(fs);
+    if (ret != OK) {
+        fprintf(stderr, "Failed to unmount filesystem: %d\n", ret);
+        free(dd);
+        free(fs);
+        exit(-1);
+    }
+    printf("Success to unmount disk to filesystem...\n");
+
 
     ddetach(dd);
     printf("File detach from struct s_disk success...\n");
